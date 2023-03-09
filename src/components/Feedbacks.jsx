d5e266c1c6c218fc5b4bf3f6d855cbeb48e3bcb3
hammadsohail <muhammadhammad848@gmail.com>
2023-03-09 19:12:33 +0500

    first commit

---
+++ src/components/Feedbacks.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
+
+
+const FeedbackCard = ({
+  index,
+  testimonial,
+  name,
+  designation,
+  company,
+  image,
+}) => {
+
+  return (
+
+    //for motion transaction effect of string motion.div is used
+
+    <motion.div
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    // bgblack2000 is used for box of testimonial
+    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+  >
+    <p className='text-white font-black text-[48px]'>"</p>
+
+    <div className='mt-1'>
+      {/* tracking wider is used for making text space */}
+      <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+
+      <div className='mt-7 flex justify-between items-center gap-1'>
+        <div className='flex-1 flex flex-col'>
+          <p className='text-white font-medium text-[16px]'>
+            <span className='blue-text-gradient'>@</span> {name}
+          </p>
+          <p className='mt-1 text-secondary text-[12px]'>
+            {designation} of {company}
+          </p>
+        </div>
+
+        <img
+          src={image}
+          alt={`feedback_by-${name}`}
+          className='w-10 h-10 rounded-full object-cover'
+        />
+      </div>
+    </div>
+  </motion.div>
+  )
+
+}
+
+const Feedbacks = () => {
+  return (
+    // main color is black-100
+    <div className={`mt-12 bg-black-100 rounded-[20px]`}>
+      {/* this teritary used for dark blue effect benith the main color section */}
+    <div
+      className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
+    >
+      <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+
+      </div>
+      {/* -20mt for effect of getting outside the box */}
+
+      <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
+      </div>
+
+
+      </div>
+  )
+}
+
+export default SectionWrapper(Feedbacks, "");
